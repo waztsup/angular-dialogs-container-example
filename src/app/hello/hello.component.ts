d2722b77c9ca108f57956af452fa6befb81c1a11
waztsup <waztfm@gmail.com>
2020-05-25 13:00:45 +0300

    Fix subscription issues

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,23 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { DialogsService } from '../services/dialogs.service'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
   styles: [`h1 { font-family: Lato; }`]
 })
-export class HelloComponent  {
+export class HelloComponent implements OnDestroy {
+
   @Input() name: string;
+  private subscription: Subscription;
 
   constructor(private dialogsService: DialogsService) {}
 
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
   openModal(id: string, params): void {
-    console.log(id);
-    this.dialogsService.open(id, params)
-      .subscribe(result => {
-        if (result.id === id) {
-          alert(JSON.stringify(result.result));
+    const open = this.dialogsService.open(id, params)
+    if (!this.subscription) {
+      this.subscription = open.subscribe(result => {
+        const message = JSON.stringify(result.result);
+        if (result.id === 'modal-1') {
+          alert(message);
+        } else {
+          console.log(message);
         }
       });
+    }
+      
   }
 }
